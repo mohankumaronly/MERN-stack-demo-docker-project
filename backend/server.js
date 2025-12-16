af71f07a1a4c0d0cc7d381ca23111ebf0225fee4
mohankumaronly <mohankumaronly81@gmail.com>
2025-12-16 15:09:51 +0530

    backend dockerization done

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
+app.get('/', (request, response) => {
+    response.status(200).send("Welcome to Rockranger's Project");
+})
+
 const StartServer = async () => {
-    try{
+    try {
         await databaseConnection(MONGODB_URL);
         app.listen(PORT, () => {
             console.log(`server is running at http://localhost:${PORT}`);
         })
-    }catch(error){
+    } catch (error) {
         console.error('server is failed to connect', error.message);
         process.exit(1);
     }
